9558e374bf7d8cec0b672c2879f33ea28cc5c65c
Rooster123872 <rsrenatoalves@gmail.com>
2026-05-15 03:58:16 -0400

    meu primeiro programa em C

---
+++ program.c
@@ -0,0 +1,16 @@
+//Meu primeiro programa em C, GG
+#include <stdio.h>
+//isso e impossivel, que linguagem dificil
+int main(void){
+
+	char text[10];
+
+	printf ("aura67 programacao\n");
+
+	printf ("Digite o seu nome \n");
+	fgets(text,10,stdin);
+
+	printf ("nome: %s", text);
+	return 0;
+
+}
